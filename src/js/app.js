38afbbd6a86c850ecd160597317adb5595c98775
thayari <alex.silvanovich@gmail.com>
2020-03-11 18:12:32 +0300

    sse

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,33 @@
 const start = document.querySelector('.start');
+const messages = document.querySelector('.messages')
 
 start.addEventListener('click', () => {
   const eventSource = new EventSource("http://localhost:7070/sse");
 
   console.log(eventSource)
 
-  eventSource.addEventListener('message', (evt) => {
-    console.log(evt);
-    messages.push(evt.data);
+  eventSource.addEventListener('message', (event) => {
+    console.log(event);
+    const message = JSON.parse(event.data);
+    let iconType = '';
+    if (message.type === 'goal') {
+      iconType = './src/img/soccer.svg';
+    } else if (message.type === 'freekick') {
+      iconType = './src/img/exclamation.svg';
+    }
+    const elem = document.createElement('div');
+    elem.classList.add('message-item');
+    elem.innerHTML = `<div class="message-date">${message.date}</div>
+    <div class="message-body"><img class="icon" src="${iconType}"><p class="message-text">${message.text}</p></div>`
+    messages.insertAdjacentElement('beforeend', elem);
+    elem.scrollIntoView();
   });
 
-  eventSource.addEventListener('open', (evt) => {
+  eventSource.addEventListener('open', (event) => {
     console.log('connected');
   });
 
-  eventSource.addEventListener('error', (evt) => {
+  eventSource.addEventListener('error', (event) => {
     console.log('error');
   });
 
